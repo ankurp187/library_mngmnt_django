9eb42028b269d2de980c53ac114c9378f0ff71fa
Ankur Pandey <ankurp187@gmail.com>
2024-06-17 00:25:50 +0530

    Added limit to book assignment

--- book/urls.py
+++ book/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path("unassign_book_def/", views.unassignBookDef, name="unassign_book_def"),
     path("allAssignments/",views.allAssignmentsView.as_view(),name="all_assign"),
     path("curr_Assignments/",views.currAssignmentsView.as_view(),name="curr_assign"),
-    path("stud_Assignments/<str:stud_id>/",views.studAssignmentsView.as_view(),name="stud_assign")
+    path("stud_Assignments/<str:stud_id>/",views.studAssignmentsView.as_view(),name="stud_assign"),
+    path("assignment_error/<str:stud_id>/",views.assignmentErrorView.as_view(),name="assignment_error"),
 ]
 
 
